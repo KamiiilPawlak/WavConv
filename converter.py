d0be825a87179241b3c4cc3e344f889b8ad561a4
KamiiilPawlak <kamil.partyk.pawlak@gmail.com>
2025-06-23 11:54:23 +0200

    Implemented audio format validation Added error handling for unsupported file formats Program aborts on unsupported format Used try-except to validate audi output format

--- converter.py
+++ converter.py
@@ -4,10 +4,27 @@ import sys
 
 input_file = "audio_samples/test_c.m4a"
 
+Format_audio = ['wav', 'mp3', 'm4a', 'flac', 'aiff', 'ogg', 'amr']
+
 output_dir = "audio_conv"
 name_convert_file = input("Nazwa pliku po konwersji (bez rozszerzenia): ")
-name_format = input("Do jakiego formatu przekonwertować (np. mp3, wav, ogg): ").lower()
-output_file = os.path.join(output_dir, f"{name_convert_file}.{name_format}")
+
+try:
+    name_format = input("Do jakiego formatu przekonwertować: ").lower()
+
+    if name_format not in Format_audio:
+        raise ValueError(f"Format '{name_format}' nie jest obsługiwany.")
+
+    output_file = os.path.join(output_dir, f"{name_convert_file}.{name_format}")
+    print(f"Plik zostanie zapisany jako: {output_file}")
+
+except ValueError as e:
+    print("Błąd:", e)
+    print(f"Dozwolone formaty to: {', '.join(Format_audio)}")
+    sys.exit(1)
+
+
+
 
 def convert_audio(input_path, output_path, output_format):
     if not os.path.exists(output_dir):
